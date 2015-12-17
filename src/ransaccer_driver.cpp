a485c07528f915ba514ecfe93f2c125c2c6515c0
alrekr <malthe.s@gmail.com>
2015-12-17 15:08:23 +0100

    Driver written, CMakeLists changed, ready test

---
+++ src/ransaccer_driver.cpp
@@ -0,0 +1,18 @@
+#include "ransaccer.h"
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+    Point center;
+    Mat image = imread("../src/marker_corny/marker_corny_01.png", CV_LOAD_IMAGE_COLOR);
+    ransaccer rs;
+    rs.assign(image);
+    center = rs.extract();
+    cout << center << endl;
+    return 0;
+}
