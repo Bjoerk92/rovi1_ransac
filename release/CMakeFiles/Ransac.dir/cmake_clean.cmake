a52c1d736d81b74a3d728f055cf8fd3da8d54812
Bjoerk92 <anders.bjoerk.unf@gmail.com>
2015-12-17 13:41:29 +0100

    first commit

---
+++ release/CMakeFiles/Ransac.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Ransac.dir/Ransac.cpp.o"
+  "Ransac.pdb"
+  "Ransac"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Ransac.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
